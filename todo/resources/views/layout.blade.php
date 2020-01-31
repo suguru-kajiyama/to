1603cfd9ec21e70e5b0ba76dde1b2d06530f30aa
suguru-kajiyama <kajgoo0920@gmail.com>
2020-01-31 03:05:48 +0000

    t

---
+++ todo/resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Task List</title>
+    <link rel='stylesheet' href='//fonts.googleapis.com/css?familiy=Roboto:300,300italic,700,700italic'>
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.xss/master/normalize.css">
+    <link rel="stylesheet" href="//css.rawgit.com/milligram.min.css">
+  </head>
+  <body>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
+</html>
